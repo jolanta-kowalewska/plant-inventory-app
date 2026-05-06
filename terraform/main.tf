ea8ae96d5ba99636be33fa18ee89d2bad676b421
Jolanta <brzojr@gmail.com>
2026-05-06 13:44:49 +0200

    frontend deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -936,3 +936,94 @@ resource "aws_lambda_function" "update_task" {
 
 
 
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "${var.project_name}-frontend-${local.environment}"
+  tags   = local.common_tags
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend_public_access" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+}
+
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  depends_on = [aws_cloudfront_distribution.frontend]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        Service = "cloudfront.amazonaws.com"
+      }
+      Action   = "s3:GetObject"
+      Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
+      Condition = {
+        StringEquals = {
+          "AWS:SourceArn" = aws_cloudfront_distribution.frontend.arn
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_cloudfront_origin_access_control" "frontend_oac" {
+  name                              = "default-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "frontend" {
+  origin {
+    domain_name              = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.frontend_oac.id
+    origin_id                = aws_s3_bucket.frontend_bucket.id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Plant App CloudFront distribution"
+  default_root_object = "login.html"
+
+ 
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.frontend_bucket.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = local.common_tags
+
+  viewer_certificate {
+   cloudfront_default_certificate = true
+}
+}
+
