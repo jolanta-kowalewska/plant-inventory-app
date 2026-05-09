9ee5daf275d92ac1a073c5321d5889e4624d7fe0
Jolanta <brzojr@gmail.com>
2026-05-09 19:20:43 +0200

    fix issue with plants_post method

--- terraform/main.tf
+++ terraform/main.tf
@@ -280,13 +280,6 @@ resource "aws_api_gateway_resource" "translate" {
   path_part   = "translate"
 }
 
-resource "aws_api_gateway_method" "plants_post" {
-  rest_api_id   = aws_api_gateway_rest_api.plant_api.id
-  resource_id   = aws_api_gateway_resource.plants.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
 resource "aws_api_gateway_method" "translate_post" {
   rest_api_id   = aws_api_gateway_rest_api.plant_api.id
   resource_id   = aws_api_gateway_resource.translate.id
